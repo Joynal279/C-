0d25e9d8273ed12a85eea374cc3ea74779accd08
joynal <joynalcu7@gmail.com>
2019-07-11 05:04:08 +0600

    Array file added

---
+++ Array/Array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+
+    class arra
+    {
+        public void fun(int[] arr1)
+        {
+            for(int i = 0; i <= arr1.Length; i++)
+            {
+                Console.WriteLine(arr1[i]);
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            arra ar = new arra();
+            int[] arr1 = { 1, 2, 3, 4, 5, 6 };
+            int[] arr2 = { 6,7,8, 9 };
+            ar.fun(arr1);
+            Console.ReadKey();
+        }
+    }
+}
