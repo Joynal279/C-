d0ac80c2ded6d0f40d9c747e6d1c4f4931f6bb10
joynal <joynalcu7@gmail.com>
2019-07-11 23:56:07 +0600

    Arr object added

---
+++ Arr/Arr/Object.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arr
+{
+    class A
+    {
+        public int id;
+        public string name;
+        public void insert(int i,string n)
+        {
+            id = i;
+            name = n;
+        }
+        public void display()
+        {
+            Console.WriteLine("id :" + id + " name :" + name);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj = new A();
+            obj.insert(23, "joynal");
+            obj.display();
+            Console.ReadKey();
+
+        }
+    }
+}
